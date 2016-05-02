baea48528ea9770ac3fc0782265b464ba7ca6ac3
Víctor Cañizares <victor.canizares.mata@gmail.com>
2016-05-02 21:03:50 +0200

    Fixed bugs in DatePeriod class

--- src/Date/DatePeriod.php
+++ src/Date/DatePeriod.php
@@ -1,5 +1,5 @@
 <?php
-namespace Aoa\Util;
+namespace Aoa\Date;
 
 /**
  * Class DatePeriod
@@ -17,15 +17,21 @@ class DatePeriod
      */
     private $endDate;
 
-    public function __construct($fromDay, $fromMonth, $toDay, $toMonth)
+    public function __construct($fromDay, $fromMonth, $toDay, $toMonth, $year = null)
     {
-        $year = (new \DateTime())->format('Y');
-        $this->startDate = (new \DateTime())->setTime($year, $fromMonth, $fromDay);
-        $this->endDate = (new \DateTime())->setTime($year, $toDay, $toMonth);
+        $year = isset($year) ? $year : (new \DateTime())->format('Y');
+        $this->startDate = (new \DateTime())->setDate($year, $fromMonth, $fromDay)->setTime(0, 0, 0);
+        $this->endDate = (new \DateTime())->setDate($year, $toMonth, $toDay)->setTime(23, 59, 59);
     }
 
+    /**
+     * Check if the received date is in period
+     *
+     * @param \DateTime $date
+     * @return bool
+     */
     public function inPeriod(\DateTime $date)
     {
-        
+        return $date >= $this->startDate && $date <= $this->endDate;
     }
 }
