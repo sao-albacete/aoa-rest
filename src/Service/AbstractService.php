de2dd582ab1f4bb5e7fc7e8b90b563b4d8c0be16
Víctor Cañizares <victor.canizares.mata@gmail.com>
2016-05-02 13:59:11 +0200

    Added new beginTransaction, commit and rollback methods to AbstractService

--- src/Service/AbstractService.php
+++ src/Service/AbstractService.php
@@ -28,4 +28,32 @@ abstract class AbstractService implements LoggerAwareInterface
     {
         $this->em = $em;
     }
+
+    /**
+     * Begins a new transaction
+     */
+    final protected function beginTransaction()
+    {
+        $this->em->beginTransaction();
+    }
+
+    /**
+     * Commits current transaction
+     */
+    final protected function commit()
+    {
+        $this->em->flush();
+        $this->em->commit();
+    }
+
+    /**
+     * Rollbacks current transaction if any
+     */
+    final protected function rollback()
+    {
+        if ($this->em->getConnection()->isTransactionActive()) {
+            $this->em->rollback();
+            $this->em->close();
+        }
+    }
 }
